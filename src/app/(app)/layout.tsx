88a222e2bd587250271b5a11a904c5f4db7da273
DexterStorey <dexter@dexterstorey.com>
2025-06-04 17:43:27 -0400

    home page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,15 @@
 import { getSession } from '~/auth/actions'
 import { ClientAuthProvider } from '~/auth/client'
+import { Nav } from '~/components/nav'
 import { Layout } from '~/ui'
 
 export default async function AppLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ClientAuthProvider session={await getSession({ redirectUnauthorized: '/signin' })}>
-			<Layout>{children}</Layout>
+			<Layout>
+				<Nav />
+				{children}
+			</Layout>
 		</ClientAuthProvider>
 	)
 }
--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,13 +1,37 @@
 'use client'
 
-import { Account } from '~/components/account'
-import { Chat } from '~/components/chat'
-
 export default function () {
 	return (
-		<>
-			<Account />
-			<Chat />
-		</>
+		<div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+			<h1>Agents</h1>
+
+			<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+				<h3>
+					<a href="/addition-agent">Addition Agent</a>
+				</h3>
+				<p>Ultra simple demo of a tool-using agent using server actions.</p>
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+				<h3>
+					<a href="/weather-agent">Weather Agent</a>
+				</h3>
+				<p>A tool use agent that uses events to show the progress of the agent.</p>
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+				<h3>
+					<a href="/research-agent">Research Agent</a>
+				</h3>
+				<p>An agent that has tools AND a custom response format</p>
+			</div>
+			<div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '10px' }}>
+				<h3>
+					<a href="/db-agent">Db Agent</a>
+				</h3>
+				<p>
+					An agent that uses the <a href="https://github.com/rubriclab/actions">@rubriclab/actions</a>{' '}
+					package to interact with a database.
+				</p>
+			</div>
+		</div>
 	)
 }
